eb1d7cb7df2d044a0f331cd680147029c1c3409d
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-11-27 22:52:36 +0100

    Staatsexamensaufgaben in Unterordner verschieben

--- .scripts/move-scans.js
+++ .scripts/move-scans.js
@@ -5,7 +5,7 @@ var glob = require("glob")
 const fs = require('fs')
 const path = require('path')
 
-glob("../Staatsexamen/**/*-*.txt", function (er, files) {
+glob("../Staatsexamen/**/*-*.pdf", function (er, files) {
   for (const file of files) {
     moveFile(file)
   }
@@ -13,7 +13,8 @@ glob("../Staatsexamen/**/*-*.txt", function (er, files) {
 
 function moveFile (oldPath) {
   console.log(oldPath)
-  let newPath = oldPath.replace(/(\d\d\d\d)-(\d\d)\.txt/, '$1/$2/OCR.txt')
+  //let newPath = oldPath.replace(/(\d\d\d\d)-(\d\d)\.txt/, '$1/$2/OCR.txt')
+  let newPath = oldPath.replace(/(\d\d\d\d)-(\d\d)\.pdf/, '$1/$2/Scan.pdf')
   console.log(newPath)
   console.log(path.dirname(newPath))
   fs.mkdirSync(path.dirname(newPath), { recursive: true })
