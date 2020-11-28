34546a4b70f63f0eac21c5f956c37c3c4e361b4e
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-11-28 07:16:51 +0100

    Skript zum Erstellen von Examens-Aufgaben

---
+++ .scripts/create-question.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+const path = require('path')
+const fs = require('fs')
+const childProcess = require('child_process')
+
+function usage() {
+  console.log('Usage: create-question.js 66116:2018:03 1 1 1')
+  console.log('66116:2018:03 1 1 1 -> 66116/2018/03/Thema-1/Teilaufgabe-1/Aufgabe-1.tex')
+  process.exit(1)
+}
+
+if (process.argv.length !== 6) {
+  usage()
+}
+
+const examen = process.argv[2].split(':')
+const examensNummer = examen[0]
+const jahr = examen[1]
+const monat = examen[2]
+
+if (!examensNummer || !jahr || !monat) {
+  usage()
+}
+
+const vorlage =
+`\\documentclass{lehramt-informatik-minimal}
+\\InformatikPakete{}
+\\begin{document}
+
+\\end{document}
+`
+
+const thema = `Thema-${process.argv[3]}`
+const teilAufgabe = `Teilaufgabe-${process.argv[4]}`
+const aufgabe = `Aufgabe-${process.argv[5]}.tex`
+
+const zielPfad = path.join(__dirname, '..', 'Staatsexamen', examensNummer, jahr, monat, thema, teilAufgabe, aufgabe)
+console.log(zielPfad)
+
+fs.mkdirSync(path.dirname(zielPfad), { recursive: true })
+fs.writeFileSync(zielPfad, vorlage, { encoding: 'utf-8' })
+
+const subprocess = childProcess.spawn('/usr/bin/code', [zielPfad], {
+  detached: true,
+  stdio: 'ignore'
+})
+
+subprocess.unref()
