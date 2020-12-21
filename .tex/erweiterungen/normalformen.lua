36811d9265de260f72c64dedce2e6d7ccfa0f606
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2020-12-21 23:28:12 +0100

    Normalformen

--- .tex/erweiterungen/normalformen.lua
+++ .tex/erweiterungen/normalformen.lua
@@ -1,18 +1,39 @@
-function text_in_buchstaben_teilen (text)
+-- http://lua-users.org/wiki/SplitJoin
+local function split(str, pat)
+  local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+  local fpat = "(.-)" .. pat
+  local last_end = 1
+  local s, e, cap = str:find(fpat, 1)
+  while s do
+     if s ~= 1 or cap ~= "" then
+        table.insert(t, cap)
+     end
+     last_end = e+1
+     s, e, cap = str:find(fpat, last_end)
+  end
+  if last_end <= #str then
+     cap = str:sub(last_end)
+     table.insert(t, cap)
+  end
+  return t
+end
+
+local function text_in_buchstaben_teilen (text)
   local ergebnis = {}
-  for buchstabe in text:gmatch(".") do
-    table.insert(ergebnis, buchstabe)
+  if string.find(text, ',') then
+    ergebnis = split(text, ' *, *')
+  else
+    for buchstabe in text:gmatch(".") do
+      table.insert(ergebnis, buchstabe)
+    end
   end
   return ergebnis
 end
 
 return {
   teilen = function (eingang)
-    print('eingang')
-    print(eingang)
     local buchstaben = text_in_buchstaben_teilen(eingang)
     local ergebnis = table.concat(buchstaben, ', ')
-    print(ergebnis)
     return ergebnis
   end
 }
