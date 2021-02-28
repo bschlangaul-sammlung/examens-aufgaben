55eefba99f26900c8ba493eeb4c225a15c584434
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-02-28 16:48:42 +0100

    Potenzmengenkonstruktion

--- .tex/erweiterungen/formale-sprachen.lua
+++ .tex/erweiterungen/formale-sprachen.lua
@@ -1,33 +1,25 @@
--- http://lua-users.org/wiki/SplitJoin
-local function split(str, pat)
-  local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-  local fpat = "(.-)" .. pat
-  local last_end = 1
-  local s, e, cap = str:find(fpat, 1)
-  while s do
-     if s ~= 1 or cap ~= "" then
-        table.insert(t, cap)
-     end
-     last_end = e+1
-     s, e, cap = str:find(fpat, last_end)
-  end
-  if last_end <= #str then
-     cap = str:sub(last_end)
-     table.insert(t, cap)
-  end
-  return t
-end
+local helfer = require('lehramt-informatik-helfer')
 
 return {
   erzeuge_tiefgestellt = function (eingang)
     local ausgabe = string.gsub(eingang, '(%a)(%d)', '%1\\sb{%2}')
-    return tex.print(ausgabe)
+    tex.print(ausgabe)
+  end,
+
+  erzeuge_zustandsnamens_liste = function (buchstabe, nummern)
+    local nummern_tabelle = helfer.split(nummern, ',')
+    for index, nr in ipairs(nummern_tabelle) do
+      -- \sb{} damit der Unterstrich vermieden wird, denn der wird
+      -- latex3 Funktionen umdefiniert.
+      nummern_tabelle[index] = buchstabe .. '\\sb{' .. nr .. '}'
+    end
+    tex.print(table.concat(nummern_tabelle, ', '))
   end,
 
   produktions_regeln = function (eingang)
     print(eingang)
     local ausgabe = ''
-    local regeln = split(eingang, ',')
+    local regeln = helfer.split(eingang, ',')
     for index, value in ipairs(regeln) do
       value = value:gsub('->', '&\\rightarrow')
       value = value:gsub('|', '\\,|\\,')
