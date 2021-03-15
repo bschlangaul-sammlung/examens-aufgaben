159fd90174e6c4bd1e859408c4a5d1fb821e0e43
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-03-15 22:24:03 +0100

    Normalformen

--- .tex/erweiterungen/normalformen.lua
+++ .tex/erweiterungen/normalformen.lua
@@ -15,23 +15,33 @@ end
 
 -- param eingang: A, B
 -- return: { A, B }
-local function formatiere_menge (eingabe)
+-- 'nichts' wird durch \emptyset ersetzt
+local function setze_menge (eingabe)
+
+  eingabe = eingabe:gsub('nichts', '\\emptyset{}')
   local elemente = helfer.split(eingabe, '%s*,%s*')
   return '\\{ ' .. table.concat(elemente, ', ') .. ' \\}'
 end
 
-local function formatiere_funktionale_abhaengigkeit(eingabe)
+local function setze_funk_abhaengigkeit(eingabe, fuer_mathe)
+  if fuer_mathe == nil then
+    fuer_mathe = true
+  end
   local seiten = helfer.split(eingabe, '%s*->%s*')
   local linke_seite = seiten[1]
   local rechte_seite = seiten[2]
-  return formatiere_menge(linke_seite) .. ' $\\rightarrow$ ' .. formatiere_menge(rechte_seite)
+  if fuer_mathe then
+    return setze_menge(linke_seite) .. ' $\\rightarrow$ ' .. setze_menge(rechte_seite)
+  else
+    return '$' .. setze_menge(linke_seite) .. ' \\rightarrow ' .. setze_menge(rechte_seite) .. '$'
+  end
 end
 
-local function formatiere_funktionale_abhaengigkeiten(eingabe)
+local function drucke_funk_abhaengigkeiten(eingabe)
   local abhaengigkeiten = helfer.split(eingabe, '%s*;%s*')
   local ausgabe = ''
   for index, abhaengigkeit in ipairs(abhaengigkeiten) do
-    ausgabe = ausgabe .. '\\item ' .. formatiere_funktionale_abhaengigkeit(abhaengigkeit) .. ' '
+    ausgabe = ausgabe .. '\\item ' .. setze_funk_abhaengigkeit(abhaengigkeit) .. ' '
   end
 
   ausgabe = '\\begin{compactitem}' .. ausgabe .. '\\end{compactitem}'
@@ -46,6 +56,8 @@ return {
     local ergebnis = table.concat(buchstaben, ', ')
     return ergebnis
   end,
-
-  formatiere_funktionale_abhaengigkeiten = formatiere_funktionale_abhaengigkeiten,
+  drucke_funk_abhaengigkeit = function (eingabe, fuer_mathe)
+    tex.print(setze_funk_abhaengigkeit(eingabe, fuer_mathe))
+  end,
+  drucke_funk_abhaengigkeiten = drucke_funk_abhaengigkeiten,
 }
