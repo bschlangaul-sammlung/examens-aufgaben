dcbd64793ad9a3f8bd6ed2227ad1bd37fb7cbad1
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-03-22 19:21:25 +0100

    Kontextfreie Sprache

--- .tex/erweiterungen/formale-sprachen.lua
+++ .tex/erweiterungen/formale-sprachen.lua
@@ -6,11 +6,17 @@ return {
     tex.print(ausgabe)
   end,
 
+  formatiere_ableitung = function (eingang)
+    eingang = eingang:gsub('nichts', '$\\emptyset$')
+    eingang = eingang:gsub('->', '$\\rightarrow$')
+    tex.print(eingang)
+  end,
+
   erzeuge_zustandsnamens_liste = function (buchstabe, nummern)
     local nummern_tabelle = helfer.split(nummern, ',')
     for index, nr in ipairs(nummern_tabelle) do
-      -- \sb{} damit der Unterstrich vermieden wird, denn der wird
-      -- latex3 Funktionen umdefiniert.
+      -- \sb{} damit der Unterstrich vermieden wird, denn der wird in
+      -- LaTeX3 Funktionen umdefiniert.
       nummern_tabelle[index] = buchstabe .. '\\sb{' .. nr .. '}'
     end
     tex.print(table.concat(nummern_tabelle, ', '))
