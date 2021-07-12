02184cd2efc9b0ed4c1811d155bcec5ce07cb21e
Hermine Blschlangaul <hermine.bschlangaul@gmx.net>
2021-07-12 09:00:07 +0200

    SYntax

--- .tex/pakete/syntax.lua
+++ .tex/pakete/syntax.lua
@@ -1,3 +1,18 @@
+-- Relativer Pfad:
+-- graph/Knoten.java
+
+-- Relativer Repo(sitory)-Pfad:
+-- src/main/java/org/bschlangaul/graph/Knoten.java
+
+-- Absoluter (Dateisystem)-Pfad:
+-- /data/github/hbschlang/lehramt-informatik/src/main/java/org/bschlangaul/graph/Knoten.java
+
+-- Github-URL:
+-- https://github.com/hbschlang/Java-Didaktik-Beispiele/blob/main/src/main/java/org/bschlangaul/graph/Knoten.java
+
+-- Github-Raw-URL:
+-- https://raw.githubusercontent.com/hbschlang/Java-Didaktik-Beispiele/main/src/main/java/org/bschlangaul/graph/Knoten.java
+
 KONF = {
   -- \LehramtInformatikRepository
   tex_repo_lokaler_pfad = nil,
@@ -13,12 +28,6 @@ KONF = {
 
   -- \LehramtInformatikGithubCodeRepo
   github_code_repo = 'hbschlang/Java-Didaktik-Beispiele',
-
-  -- src/main/java/org/bschlangaul
-  code_main = 'src/main/java/org/bschlangaul',
-
-  -- src/test/java/org/bschlangaul
-  code_test = 'src/test/java/org/bschlangaul',
 
   -- \LehramtInformatikGitBranch
   git_branch = 'main',
@@ -41,43 +50,67 @@ local function monat_zu_jahreszeit(monat)
   return ausgabe
 end
 
-local function gib_github_url(relativer_pfad)
-  return KONF.github_raw_domain .. '/' ..
-    KONF.github_code_repo .. '/' ..
-    KONF.git_branch ..'/' ..
-    KONF.code_main .. '/'
-    .. normalisiere_klasse(relativer_pfad)
+--- Gib den zum Git-Repository (hbschlang/Java-Didaktik-Beispiele)
+--  relativen Pfad zurück.
+--
+-- @tparam str relativer_pfad Ein relativer Pfad zu einer Java-Datei
+--   (relativ zu src/main/java/org/bschlangaul). Kann die Endung .java
+--   enthalten. Die Endung kann aber auch weggelassen werden.
+-- @tparam bool ist_test Ob die Datei in src/main/java/org/bschlangaul
+--   oder src/test/java/org/bschlangaul liegt.
+
+-- @return zum Beispiel src/main/java/org/bschlangaul/examen/Aufgabe1.java
+local function gib_relativen_repo_pfad(relativer_pfad, ist_test)
+  local main_oder_test = 'main'
+  if ist_test == true then
+    main_oder_test = 'test'
+  end
+
+  return
+    'src/' ..
+    main_oder_test ..
+    '/java/org/bschlangaul/' ..
+    normalisiere_klasse(relativer_pfad) ..
+    '.java'
 end
 
 --- Gib den absoluten Pfad zu einer Java-Datei.
 --
 -- @tparam str relativer_pfad Ein relativer Pfad zu einer Java-Datei
 --   (relativ zu src/main/java/org/bschlangaul). Kann die Endung .java
---   enthalten, muss aber nicht
+--   enthalten. Die Endung kann aber auch weggelassen werden.
+-- @tparam bool ist_test Ob die Datei in src/main/java/org/bschlangaul
+--   oder src/test/java/org/bschlangaul liegt.
 
 -- @return Der absolute lokale Dateipfad
 local function gib_absoluten_pfad(relativer_pfad, ist_test)
-  local relativer_code_pfad
-  if ist_test == true then
-    relativer_code_pfad = KONF.code_test
-  else
-    relativer_code_pfad = KONF.code_main
-  end
-  return KONF.tex_repo_lokaler_pfad .. '/Code/' ..
-  relativer_code_pfad .. '/' ..
-    normalisiere_klasse(relativer_pfad) .. '.java'
+  return KONF.tex_repo_lokaler_pfad ..
+    '/Code/' ..
+    gib_relativen_repo_pfad(relativer_pfad, ist_test)
 end
 
 -- Examen
 
-local function gib_relativen_examens_pfad(nummer, jahr, monat, pfad)
+local function gib_examens_pfad(nummer, jahr, monat, relativer_examens_pfad)
   return 'examen/examen_' .. nummer ..
     '/jahr_' .. jahr .. '/' ..
-    monat_zu_jahreszeit(monat) .. '/' .. normalisiere_klasse(pfad) .. '.java'
+    monat_zu_jahreszeit(monat) .. '/' .. relativer_examens_pfad
+end
+
+-- Github URL
+
+local function gib_github_url(relativer_repo_pfad)
+  return KONF.github_domain .. '/' ..
+    KONF.github_code_repo .. '/blob/' ..
+    KONF.git_branch ..'/' .. relativer_repo_pfad
 end
 
 local function gib_github_examen_url(nummer, jahr, monat, pfad)
-  return gib_github_url(gib_relativen_examens_pfad(nummer, jahr, monat, pfad))
+  return gib_github_url(
+    gib_relativen_repo_pfad(
+      gib_examens_pfad(nummer, jahr, monat, pfad), false
+    )
+  )
 end
 
 local export = {
@@ -87,28 +120,48 @@ local export = {
 
   -- absoluten pfad
 
-  drucke_absoluten_pfad = function(relativer_pfad)
-    tex.print(gib_absoluten_pfad(relativer_pfad))
+  drucke_absoluten_pfad = function(relativer_pfad, ist_test)
+    tex.print(
+      gib_absoluten_pfad(relativer_pfad, ist_test)
+    )
   end,
 
   drucke_absoluten_examens_pfad = function(nummer, jahr, monat, pfad)
-    tex.print(gib_absoluten_pfad(gib_relativen_examens_pfad(nummer, jahr, monat, pfad)))
+    tex.print(
+      gib_absoluten_pfad(
+        gib_examens_pfad(nummer, jahr, monat, pfad), false
+      )
+    )
   end,
 
   -- relativen
 
-  drucke_relativen_pfad = function(relativer_pfad)
-    tex.print(gib_relativen_pfad(relativer_pfad))
+  drucke_relativen_repo_pfad = function(relativer_pfad, ist_test)
+    tex.print(
+      gib_relativen_repo_pfad(relativer_pfad, ist_test)
+    )
   end,
 
-  drucke_relativen_examens_pfad = function(nummer, jahr, monat, pfad)
-    tex.print(gib_relativen_examens_pfad(nummer, jahr, monat, pfad))
+  drucke_relativen_examens_repo_pfad = function(nummer, jahr, monat, pfad)
+    tex.print(
+      gib_relativen_repo_pfad(
+        gib_examens_pfad(nummer, jahr, monat, pfad),
+        false
+      )
+    )
   end,
 
   -- url
 
-  drucke_github_url = function(relativer_pfad)
-    tex.print(gib_github_url(relativer_pfad))
+  drucke_github_url = function(relativer_pfad, ist_test)
+    tex.print(
+      gib_github_url(
+        gib_relativen_repo_pfad(
+          relativer_pfad,
+          ist_test
+        )
+      )
+    )
   end,
 
   drucke_github_examens_url = function(nummer, jahr, monat, pfad)
